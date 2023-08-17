cad40ba62610597a1f8af03cad11ba64a23cfaf9
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2023-08-17 20:34:20 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -6,13 +6,13 @@ const options = {
     url: BASE_URL,
 
     params: {
-        maxResults: 50,
+        maxResults: '50',
     },
 
     headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    },
+    }
 };
 
 export const fetchFromAPI = async (url) => {
