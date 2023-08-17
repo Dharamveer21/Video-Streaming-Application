6d2e14228e5db9c1633f531c9f211522498a7d0d
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2023-08-18 00:42:12 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v3-lite.p.rapidapi.com';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
     url: BASE_URL,
 
     params: {
-
+         maxResults: '50'
     },
 
     headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'youtube-v3-lite.p.rapidapi.com'
+        'X-RapidAPI-Host': 'https://youtube-v31.p.rapidapi.com'
     }
 };
 
