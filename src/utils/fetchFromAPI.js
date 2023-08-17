ff4b5af55358d8404d0e012532f8f897c4f0d78c
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2023-08-17 20:19:07 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -6,17 +6,17 @@ const options = {
     url: BASE_URL,
 
     params: {
-        maxResults: '50'
+        maxResults: 50,
     },
 
     headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
 };
 
 export const fetchFromAPI = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
     return data;
-}
+};
